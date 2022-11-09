dd5af1bf72eb839537028815d63bcfe24f9fd98d
joewulf1 <87823227+joewulf1@users.noreply.github.com>
2022-11-08 19:23:24 -0600

    Basic of Timer
    Need to put in package

--- timer_app/lib/main.dart
+++ timer_app/lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Rando Timer',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Rando Timer'),
     );
   }
 }
@@ -29,11 +31,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counterSec = 0;
+  int _counterMin = 0;
+  late Timer _timerSeconds;
+  late Timer _timerMinutes;
+  bool _isButtonDisabled = true;
 
-  void _incrementCounter() {
+  _startTimerMin() {
+    _counterMin = randNum() - 1;
+
+    _timerMinutes = Timer.periodic(const Duration(minutes: 1), (timer) {
+      setState(
+        () {
+          if (_counterMin > 0) {
+            _counterMin--;
+          } else {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => timerDead()),
+            );
+            _timerMinutes.cancel();
+            _counterMin = 0;
+          }
+        },
+      );
+    });
+  }
+
+  _startTimerSec() {
+    _counterSec = 59;
+
+    _timerSeconds = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(
+        () {
+          if (_counterSec > 0) {
+            _counterSec--;
+          } else if (_counterMin > 0) {
+            _counterSec = 59;
+          } else {
+            _timerSeconds.cancel();
+            _counterSec = 0;
+          }
+        },
+      );
+    });
+  }
+
+  @override
+  void initState() {
+    _isButtonDisabled = false;
+  }
+
+  void disableButton() {
     setState(() {
-      _counter++;
+      _isButtonDisabled = true;
+    });
+  }
+
+  void enableButton() {
+    setState(() {
+      _isButtonDisabled = false;
     });
   }
 
@@ -44,24 +101,100 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color.fromARGB(255, 255, 255, 255),
+                border: Border.all(color: Colors.black)),
+            child: FloatingActionButton.large(
+              heroTag: "startTimer",
+              onPressed: () {
+                if (_isButtonDisabled) {
+                  null;
+                } else {
+                  _startTimerSec();
+                  _startTimerMin();
+                  _counterMin = randNum();
+                  disableButton();
+                }
+              },
+              child: _isButtonDisabled
+                  ? const Icon(Icons.block)
+                  : const Icon(Icons.timer),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "$_counterMin",
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  ":",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "$_counterSec",
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        ],
+      )),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () {
+          _timerMinutes.cancel();
+          _timerSeconds.cancel();
+          enableButton();
+        },
+        heroTag: "timerReset",
+        tooltip: 'Reset',
+        child: const Icon(Icons.refresh),
       ),
     );
+  }
+}
+
+randNum() {
+  Random random = new Random();
+  int rand = random.nextInt(20) + 4;
+  return rand;
+}
+
+class timerDead extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        content: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Text("Timer Done!"),
+        FloatingActionButton(
+            heroTag: "closePopup",
+            child: const Icon(Icons.refresh),
+            onPressed: () {
+              Navigator.pop(context);
+            })
+      ],
+    ));
   }
 }
